73db7a1ac9f9d0b6bd19b6bcee0c203afcd56885
artmg <artmg@users.noreply.github.com>
2014-01-25 10:51:05 +0000

    Update update-os-and-apps.bash

--- update-os-and-apps.bash
+++ update-os-and-apps.bash
@@ -18,13 +18,16 @@ sudo apt-get dist-upgrade -y
 # and if you want to change timeout then first edit /etc/default/grub
 sudo update-grub
 
-# if you want to enable automatic updates...
+
+# Enable automatic updates except on LiveUSBs, where they should stay on demand...
+if [[ $LUBUILD_HARDWARE_TYPE_LIVEUSB -neq TRUE ]] ; then (
 sudo apt-get install unattended-upgrades
 # credit > http://mcarpenter.org/blog/2012/08/12/ubuntu-automatic-package-updates
 sudo sh -c "echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections"
 # for solution using a deb set default config file see https://github.com/netsocDIT/serversetup/blob/master/unattended-upgrades.sh
 sudo dpkg-reconfigure -plow unattended-upgrades
 # help > https://help.ubuntu.com/community/AutomaticSecurityUpdates
+)
 
 # reboot to use any new kernel version installed
 sudo reboot
