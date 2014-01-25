b58b65d969920b7027e30e56230ec791cabee9d4
artmg <artmg@users.noreply.github.com>
2014-01-25 01:47:23 +0000

    Create update-os-and-apps.bash

---
+++ update-os-and-apps.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### UPDATES ################
+
+# Prepare for repository installs
+sudo apt-get update
+
+# update existing applications with no user interaction...
+sudo apt-get upgrade -y
+
+
+### OPTIONAL UPDATES only if you want ...
+
+# load any kernel updates
+sudo apt-get dist-upgrade -y
+
+# ensure that grub is properly updated in case of issues
+# and if you want to change timeout then first edit /etc/default/grub
+sudo update-grub
+
+# if you want to enable automatic updates...
+sudo apt-get install unattended-upgrades
+# credit > http://mcarpenter.org/blog/2012/08/12/ubuntu-automatic-package-updates
+sudo sh -c "echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections"
+# for solution using a deb set default config file see https://github.com/netsocDIT/serversetup/blob/master/unattended-upgrades.sh
+sudo dpkg-reconfigure -plow unattended-upgrades
+# help > https://help.ubuntu.com/community/AutomaticSecurityUpdates
+
+# reboot to use any new kernel version installed
+sudo reboot
+
