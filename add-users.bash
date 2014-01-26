058054dbb8dd308f09d0ba890aab08d81fc4b741
artmg <artmg@users.noreply.github.com>
2014-01-26 21:42:00 +0000

    install ecryptfs-utils

--- add-users.bash
+++ add-users.bash
@@ -6,6 +6,7 @@ adduser --encrypt-home $NEWUSER
 # credit - http://askubuntu.com/questions/132395/
 
 ## if user already created use...
+# sudo apt-get install -y ecryptfs-utils
 # sudo ecryptfs-migrate-home -u user
 ## credit - http://www.howtogeek.com/116032/
 ## log in with this user BEFORE the next reboot
