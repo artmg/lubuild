fbdaeef53affa4b7c2da8a820d0bee65ef28b574
artmg <artmg@users.noreply.github.com>
2014-05-27 00:10:25 +0100

    more details and groups

--- add-users.bash
+++ add-users.bash
@@ -1,15 +1,21 @@
 ### Users ###
 
 NEWUSER=NewUserName
+DISPLAYNAME=UserDisplayName
 
-adduser --encrypt-home $NEWUSER
+sudo adduser --encrypt-home --gecos $DISPLAYNAME,,, $NEWUSER
 # credit - http://askubuntu.com/questions/132395/
+
+### User rights  via group membership
+sudo usermod -a -G audio,video,plugdev,netdev,fuse,lpadmin,scanner $NEWUSER
 
 ## if user already created use...
 # sudo apt-get install -y ecryptfs-utils
 # sudo ecryptfs-migrate-home -u user
 ## credit - http://www.howtogeek.com/116032/
 ## log in with this user BEFORE the next reboot
+
+
 
 # If the user needs to have admin privilege they must be a ''sudoer''
 groups $NEWUSER
