8429fe1082eba0cf5a68df10db27f42067658bed
artmg <artmg@users.noreply.github.com>
2014-09-29 15:02:36 +0100

    also Lubuntu 14.10 Beta 2

--- os_version_fixes.bash
+++ os_version_fixes.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # credit - https://bugs.launchpad.net/ubuntu/+source/pcmanfm/+bug/975152/comments/17
-if [[ "${DESKTOP_SESSION} $(lsb_release -sr)" == "Lubuntu 14.04" ]] ; then 
+# still an issue in Lubuntu 14.10 (Beta 2)
+if \ 
+    [[ "${DESKTOP_SESSION} $(lsb_release -sr)" == "Lubuntu 14.04" ]] \
+    || [[ "${DESKTOP_SESSION} $(lsb_release -sr)" == "Lubuntu 14.10" ]] \
+    ; then 
   echo ### open bash scripts in Terminal from File Manager - Lub 14.04 ; 
   sudo sed -i -e 's|lxsession-default-terminal %s|x-terminal-emulator -e %s|' \
    /etc/xdg/lubuntu/libfm/libfm.conf ; 
