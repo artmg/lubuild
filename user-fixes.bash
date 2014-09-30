a31802fdc49b6d28b672c8de93c1d0ac425609ec
artmg <artmg@users.noreply.github.com>
2014-09-30 09:03:28 +0100

    Update user-fixes.bash

--- user-fixes.bash
+++ user-fixes.bash
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-====Sound controls====
+# ====Sound controls====
+# Acer One not adjusting volume with Fn VolUp and Fn VolDn keys
+# credit - http://ubuntuforums.org/archive/index.php/t-1977849.html
 
- # Acer One not adjusting volume with Fn VolUp and Fn VolDn keys
- # credit - http://ubuntuforums.org/archive/index.php/t-1977849.html
- # 
- gnome-text-editor ~/.config/openbox/lubuntu-rc.xml
- #
- # find the text   XF86AudioRaiseVolume
- # after each of the three  commands   amixer -q   insert the following text before   sset
- #   -D pulse 
- #
- openbox --reconfigure
+MODEL_NO=`sudo dmidecode -s system-product-name`
+echo $MODEL_NO
+if \
+    [[ "${MODEL_NO}" == "AO722" ]] \
+    || [[ "${MODEL_NO}" == "sample other" ]] \
+; then
+  # find the text   XF86AudioRaiseVolume
+  # after each of the three  commands   amixer -q   insert the following text before   sset
+  #   -D pulse 
+  sed -i -e 's|amixer -q sset|amixer -q -D pulse sset|' \
+  $HOME/.config/openbox/lubuntu-rc.xml ;
+
+  openbox --reconfigure
  
+fi
+
 
