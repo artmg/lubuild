0bae6b6a75c9aa9d3e194bb8e736bb1a4eb081e5
ArtMG <gh-build@artmg.org>
2021-04-01 12:57:42 +0100

    script to Loop through list of pathnames

--- help/manipulate/files-and-folders.sh
+++ help/manipulate/files-and-folders.sh
@@ -24,3 +24,24 @@ rsync -av --prune-empty-dirs --remove-source-files Source/ Target/
 find . -type d -empty -delete
 # credit https://unix.stackexchange.com/a/107556
 
+
+## Loop through list of pathnames
+
+# These are meant to be used interactively
+
+### and output num files dirs and datasize
+
+DELIM=
+cat <<EOF |
+$HOME/Documents
+$HOME/Desktop
+EOF
+
+while read line
+do
+    pushd "${line}" >nul
+    echo $line $DELIM `find .//. ! -name . -type d -print | grep -c //` $DELIM `find .//. ! -name . -type f -print | grep -c //` $DELIM `du -sh`
+    # credit https://unix.stackexchange.com/a/1126
+    popd >nul
+done
+echo pathname $DELIM folders $DELIM files $DELIM datavolume
